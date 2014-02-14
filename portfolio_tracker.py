3187cad0ceafc2886c8a6d9c95ebceb0b7343997
gobbo <robertogobbetti@gmail.com>
2014-02-14 17:18:43 -0500

    added list of random stocks to play with and learn how to display

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -1,1 +1,5 @@
 #!/usr/bin/env python
+
+#stocks are order for: title, name of the company, purchase price, latest price
+
+stocks = [["GOOG","google",1134.18,1202.80],["GRPN","Groupon",10.46,10.51],["DDD","3D Systems Corporation",66.91,73.83],["FB","Facebook",64.33,67.09]]
