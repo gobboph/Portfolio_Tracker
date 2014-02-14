d8c4877a996afeaaa6e20861f22d707a355b060c
gobbo <robertogobbetti@gmail.com>
2014-02-14 17:31:22 -0500

    added import string

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import string
+
 #stocks are order for: title, name of the company, purchase price, latest price
 
 stocks = [["GOOG","google",1134.18,1202.80],["GRPN","Groupon",10.46,10.51],["DDD","3D Systems Corporation",66.91,73.83],["FB","Facebook",64.33,67.09]]
