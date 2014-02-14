cc053a67afe65c6b8c17bfb20ec9afcc3b3c780f
gobbo <robertogobbetti@gmail.com>
2014-02-14 17:43:37 -0500

    deleted previous comment

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -3,6 +3,5 @@
 import string
 
 #stocks are order for: title, name of the company, purchase price, latest price
-#small change to try git
 
 stocks = [["GOOG","google",1134.18,1202.80],["GRPN","Groupon",10.46,10.51],["DDD","3D Systems Corporation",66.91,73.83],["FB","Facebook",64.33,67.09]]
