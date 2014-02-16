84e9f81b227e5a129f397e05b6315fcc5bb04615
gobbo <robertogobbetti@gmail.com>
2014-02-15 19:41:47 -0500

    need to make table generation and upadate two functions

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -12,6 +12,7 @@ stocks = {'Google':["GOOG","Google",100,1134.18,1202.80],\
 
 
 #desiging portfolio table
+
 portfolio = PrettyTable(['Company','Shares','Purchase','Latest','Value','Gain'])
 portfolio.align['Company'] = 'l'
 portfolio.padding_width = 1
@@ -23,7 +24,6 @@ for x in stocks:
 		stocks[x][2]*stocks[x][4],\
 		str(round((-stocks[x][3]+stocks[x][4])/stocks[x][3]*100,1))+'%'])
 
-
 print 'This is your portfolio at the moment'
 print portfolio
 
@@ -32,18 +32,17 @@ UpDate = raw_input('If you want to update it press U, otherwise admire how nice 
 if UpDate == 'U':
 	AddDel = raw_input('Do you want to (A)dd or (D)elete any stock?').upper()
 	if AddDel == 'A':
-		new_stock = str(raw_input('Write the name of the company you want to add\n'))
-		new_symbol = str(raw_input('And its new_symbol, please\n'))
-		num_share = raw_input('how many of those do you have?\n')
-		purch_price = raw_input('And how much did you pay for them?\n')
+		new_stock = str(raw_input('Write the name of the company you want to add \n'))
+		new_symbol = str(raw_input('And its new_symbol, please \n'))
+		num_share = raw_input('how many of those do you have? \n')
+		purch_price = raw_input('And how much did you pay for them? \n')
 		x='a'
 		count=1
 		while x!='Y' or x!='N'or count!=10:
 			x = raw_input('Is the price now the same as the when you bought it? (Y/N)').upper()
 			count+=1
-		if count==10:
-			print 'Well, too many times to try and press Y or N, idiot, I am shutting down'
-			break
+			if count==10:
+				print 'Well, too many times to try and press Y or N, idiot, I am shutting down'
 		if x=='Y':
 			latest_price = purch_price
 		elif x=='N':
@@ -64,8 +63,23 @@ if UpDate == 'U':
 
 	else:
 		print 'I did not undertsand, therefore exiting the program. Be more careful with typing, asshole.'
-		break
+		exit()
 
 print 'I am printing the updated table for you now, then shutting down. (No you can not update more than once at the moment)'
+
+portfolio = PrettyTable(['Company','Shares','Purchase','Latest','Value','Gain'])
+portfolio.align['Company'] = 'l'
+portfolio.padding_width = 1
+for x in stocks:
+	portfolio.add_row([stocks[x][1]+' ('+stocks[x][0]+')',\
+		stocks[x][2],\
+		stocks[x][3],\
+		stocks[x][4],\
+		stocks[x][2]*stocks[x][4],\
+		str(round((-stocks[x][3]+stocks[x][4])/stocks[x][3]*100,1))+'%'])
+
 print portfolio
-break
+
+
+exit()
+
