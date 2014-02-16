b570ad5b930d5501a1b172601994389b7e1e3c93
gobbo <robertogobbetti@gmail.com>
2014-02-16 16:46:25 -0500

    Added functions for showing portfolio and adding stock

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -3,6 +3,45 @@
 import string
 from prettytable import PrettyTable
 
+
+# !!!DEFINING RELEVANT FUNCTIONS!!!
+
+def show_portfolio(dict):
+#This works only for dictionary defined in such a way that for each key there is an array structured like this:
+#title, name of the company, number of shares, purchase price, latest price
+	portfolio = PrettyTable(['Company','Shares','Purchase','Latest','Value','Gain'])
+	portfolio.align['Company'] = 'l'
+	portfolio.padding_width = 1
+	for x in stocks:
+		portfolio.add_row([dict[x][1]+' ('+dict[x][0]+')',\
+			dict[x][2],\
+			dict[x][3],\
+			dict[x][4],\
+			dict[x][2]*dict[x][4],\
+			str(round((-dict[x][3]+dict[x][4])/dict[x][3]*100,1))+'%'])
+	print portfolio
+
+def add_stock(dict):
+#Function to add a stock to portfolio, work with the same dictionary structure as above
+	new_stock = str(raw_input('Write the name of the company you want to add \n'))
+	new_symbol = str(raw_input('And its new_symbol, please \n'))
+	num_share = int(raw_input('how many of those do you have? \n'))
+	purch_price = round(float(raw_input('And how much did you pay for them? \n')),1)
+	x = raw_input('Is the price now the same as the when you bought it? (Y/N) ').upper()
+	if x=='Y':
+		latest_price = purch_price
+	elif x=='N':
+		latest_price = round(float(raw_input('Then what is its price now?\n')),1)
+	else:
+		print 'you should have been more precise in pressing y or n, now I am shutting down'
+		exit()
+	dict[new_stock]=[]
+	dict[new_stock].append(new_symbol)
+	dict[new_stock].append(new_stock)
+	dict[new_stock].append(num_share)
+	dict[new_stock].append(purch_price)
+	dict[new_stock].append(latest_price)
+
 #stocks are order for: title, name of the company, number of shares, purchase price, latest price
 
 stocks = {'Google':["GOOG","Google",100,1134.18,1202.80],\
@@ -10,22 +49,8 @@ stocks = {'Google':["GOOG","Google",100,1134.18,1202.80],\
 '3D Systems Corporation':["DDD","3D Systems Corporation",50,66.91,73.83],\
 'Facebook':["FB","Facebook",50,64.33,67.09]}
 
-
-#desiging portfolio table
-
-portfolio = PrettyTable(['Company','Shares','Purchase','Latest','Value','Gain'])
-portfolio.align['Company'] = 'l'
-portfolio.padding_width = 1
-for x in stocks:
-	portfolio.add_row([stocks[x][1]+' ('+stocks[x][0]+')',\
-		stocks[x][2],\
-		stocks[x][3],\
-		stocks[x][4],\
-		stocks[x][2]*stocks[x][4],\
-		str(round((-stocks[x][3]+stocks[x][4])/stocks[x][3]*100,1))+'%'])
-
 print 'This is your portfolio at the moment'
-print portfolio
+show_portfolio(stocks)
 
 UpDate = raw_input('If you want to update it press U, otherwise you can (E)xit. ').upper()
 
@@ -50,11 +75,6 @@ if UpDate == 'U':
 		stocks[new_stock].append(num_share)
 		stocks[new_stock].append(purch_price)
 		stocks[new_stock].append(latest_price)
-		#print stocks[new_stock][0]
-		#print stocks[new_stock][1]
-		#print stocks[new_stock][2]
-		#print stocks[new_stock][3]
-		#print stocks[new_stock][4]
 
 	elif AddDel == 'D':
 		del_stock = raw_input('Which company have you sold? (capitalize correctly, you have the table right in fron of you)')
@@ -68,20 +88,7 @@ if UpDate == 'U':
 		exit()
 
 print 'I am printing the updated table for you now, then shutting down. (No you can not update more than once at the moment)'
-
-portfolio = PrettyTable(['Company','Shares','Purchase','Latest','Value','Gain'])
-portfolio.align['Company'] = 'l'
-portfolio.padding_width = 1
-for x in stocks:
-	portfolio.add_row([stocks[x][1]+' ('+stocks[x][0]+')',\
-		stocks[x][2],\
-		stocks[x][3],\
-		stocks[x][4],\
-		stocks[x][2]*stocks[x][4],\
-		str(round((-stocks[x][3]+stocks[x][4])/stocks[x][3]*100,1))+'%'])
-
-print portfolio
-
+show_portfolio(stocks)
 
 exit()
 
