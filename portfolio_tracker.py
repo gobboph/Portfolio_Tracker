9bee728f6fc182b28ca959a1f1cfc4e94bfe48c5
gobbo <robertogobbetti@gmail.com>
2014-02-16 16:57:06 -0500

    put main run in main() and running indefinitely until one wants to quit

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -4,6 +4,35 @@ import string
 from prettytable import PrettyTable
 
 
+#stocks are order for: title, name of the company, number of shares, purchase price, latest price
+stocks = {'Google':["GOOG","Google",100,1134.18,1202.80],\
+'Groupon':["GRPN","Groupon",100,10.46,10.51],\
+'3D Systems Corporation':["DDD","3D Systems Corporation",50,66.91,73.83],\
+'Facebook':["FB","Facebook",50,64.33,67.09]}
+
+def main():
+	UpDate = raw_input('If you want to update it press U, otherwise you can (E)xit. ').upper()
+	if UpDate == 'U':
+		AddDel = raw_input('Do you want to (A)dd or (D)elete any stock? ').upper()
+		if AddDel == 'A':
+			add_stock(stocks)
+		elif AddDel == 'D':
+			del_stock(stocks)
+		else:
+			print 'I did not undertsand, therefore exiting the program. Be more careful with typing, asshole.'
+			exit()
+	print 'This is your updated portfolio'
+	show_portfolio(stocks)
+
+
+print 'This is your portfolio'
+show_portfolio(stocks)
+while True:
+	main()
+
+
+
+
 # !!!DEFINING RELEVANT FUNCTIONS!!!
 
 def show_portfolio(dict):
@@ -42,54 +71,40 @@ def add_stock(dict):
 	dict[new_stock].append(purch_price)
 	dict[new_stock].append(latest_price)
 
-#stocks are order for: title, name of the company, number of shares, purchase price, latest price
-
-stocks = {'Google':["GOOG","Google",100,1134.18,1202.80],\
-'Groupon':["GRPN","Groupon",100,10.46,10.51],\
-'3D Systems Corporation':["DDD","3D Systems Corporation",50,66.91,73.83],\
-'Facebook':["FB","Facebook",50,64.33,67.09]}
-
-print 'This is your portfolio at the moment'
-show_portfolio(stocks)
-
-UpDate = raw_input('If you want to update it press U, otherwise you can (E)xit. ').upper()
-
-if UpDate == 'U':
-	AddDel = raw_input('Do you want to (A)dd or (D)elete any stock? ').upper()
-	if AddDel == 'A':
-		add_stock(stocks)
-		#new_stock = str(raw_input('Write the name of the company you want to add \n'))
-		#new_symbol = str(raw_input('And its new_symbol, please \n'))
-		#num_share = int(raw_input('how many of those do you have? \n'))
-		#purch_price = round(float(raw_input('And how much did you pay for them? \n')),1)
-		#x = raw_input('Is the price now the same as the when you bought it? (Y/N) ').upper()
-		#if x=='Y':
-		#	latest_price = purch_price
-		#elif x=='N':
-	#		latest_price = round(float(raw_input('Then what is its price now?\n')),1)
-	#	else:
-	#		print 'you should have been more precise in pressing y or n, now I am shutting down'
-	#		exit()
-	#	stocks[new_stock]=[]
-	#	stocks[new_stock].append(new_symbol)
-	#	stocks[new_stock].append(new_stock)
-	#	stocks[new_stock].append(num_share)
-	#	stocks[new_stock].append(purch_price)
-	#	stocks[new_stock].append(latest_price)
-
-	elif AddDel == 'D':
-		del_stock = raw_input('Which company have you sold? (capitalize correctly, you have the table right in fron of you)')
-		if del_stock not in stocks:
-			print 'You did not have that, moron, so I am shutting down now'
-		else:
-			del stocks[del_stock]
-
+def del_stock(dict):
+#Function to delete stock from portfolio
+	del_stock = raw_input('Which company have you sold? (capitalize correctly, you have the table right in front of you) ')
+	if del_stock not in stocks:
+		print 'You did not have that, moron, so I am shutting down now'
 	else:
-		print 'I did not undertsand, therefore exiting the program. Be more careful with typing, asshole.'
-		exit()
+		del stocks[del_stock]
 
-print 'I am printing the updated table for you now, then shutting down. (No you can not update more than once at the moment)'
-show_portfolio(stocks)
 
-exit()
+#stocks are order for: title, name of the company, number of shares, purchase price, latest price
+#stocks = {'Google':["GOOG","Google",100,1134.18,1202.80],\
+#'Groupon':["GRPN","Groupon",100,10.46,10.51],\
+#'3D Systems Corporation':["DDD","3D Systems Corporation",50,66.91,73.83],\
+#'Facebook':["FB","Facebook",50,64.33,67.09]}
+
+#print 'This is your portfolio'
+#show_portfolio(stocks)
+
+#def main():
+#	UpDate = raw_input('If you want to update it press U, otherwise you can (E)xit. ').upper()
+#	if UpDate == 'U':
+#		AddDel = raw_input('Do you want to (A)dd or (D)elete any stock? ').upper()
+#		if AddDel == 'A':
+#			add_stock(stocks)
+#		elif AddDel == 'D':
+#			del_stock(stocks)
+#		else:
+#			print 'I did not undertsand, therefore exiting the program. Be more careful with typing, asshole.'
+#			exit()
+#	print 'This is your updated portfolio'
+#	show_portfolio(stocks)
+
+#print 'I am printing the updated table for you now, then shutting down. (No you can not update more than once at the moment)'
+#show_portfolio(stocks)
+
+#exit()
 
