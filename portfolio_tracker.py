b0c93020bd6d3404a90ebc43b6737e897b728b31
gobbo <robertogobbetti@gmail.com>
2014-02-15 19:04:38 -0500

    switched from list to dictionary

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -5,18 +5,19 @@ from prettytable import PrettyTable
 
 #stocks are order for: title, name of the company, purchase price, latest price
 
-stocks = [["GOOG","Google",100,1134.18,1202.80],\
-["GRPN","Groupon",100,10.46,10.51],\
-["DDD","3D Systems Corporation",50,66.91,73.83],\
-["FB","Facebook",50,64.33,67.09]]
+stocks = {'Google':["GOOG","Google",100,1134.18,1202.80],\
+'Groupon':["GRPN","Groupon",100,10.46,10.51],\
+'3D Systems Corporation':["DDD","3D Systems Corporation",50,66.91,73.83],\
+'Facebook':["FB","Facebook",50,64.33,67.09]}
 
 portfolio = PrettyTable(['Company','Shares','Purchase','Latest','Value','Gain'])
 portfolio.align['Company'] = 'l'
 portfolio.padding_width = 1
-for i in range(len(stocks)):
-	portfolio.add_row([stocks[i][1]+' ('+stocks[i][0]+')',\
-		stocks[i][2],stocks[i][3],\
-		stocks[i][4],\
-		stocks[i][2]*stocks[i][4],\
-		str(round((-stocks[i][3]+stocks[i][4])/stocks[i][3]*100,1))+'%'])
+for x in stocks:
+	portfolio.add_row([stocks[x][1]+' ('+stocks[x][0]+')',\
+		stocks[x][2],\
+		stocks[x][3],\
+		stocks[x][4],\
+		stocks[x][2]*stocks[x][4],\
+		str(round((-stocks[x][3]+stocks[x][4])/stocks[x][3]*100,1))+'%'])
 print portfolio
