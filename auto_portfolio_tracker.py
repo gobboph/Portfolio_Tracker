7ad6683d5fde9a5c4b21655f5715640e0c041756
gobbo <robertogobbetti@gmail.com>
2014-02-23 00:21:54 -0500

    did I do anything new here?

--- auto_portfolio_tracker.py
+++ auto_portfolio_tracker.py
@@ -96,7 +96,7 @@ def update_price(dict):
 def create_file():
 	print 'Creating a new text file...'
 	name = raw_input('name of file: ')+'.txt'
-	new_file = open(name,'w')
+	new_file = open(name,'a')
 	new_file.close()
 	return name
 
